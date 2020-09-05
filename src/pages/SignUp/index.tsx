f574a6a23b6c83371885f4224ea554dd8319ef85
tsunoda1 <tsunoda@b2ml.com.br>
2020-09-05 15:43:19 -0300

    feat: create signin and signup pages

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
+
+import logoimg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoimg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu cadstro</h1>
+
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
